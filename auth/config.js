674eb64dfa530f9c2488d4cdb9a69cf502a2e9f9
SLAVUSworks <abdurrahmanagharabbani45@gmail.com>
2023-10-30 22:11:51 +0700

    auth

---
+++ auth/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBR-HKp0dZ4GDr4rlGKhyUjtK3I0qHBra4",
+    authDomain: "formfsi-9b033.firebaseapp.com",
+    projectId: "formfsi-9b033",
+    storageBucket: "formfsi-9b033.appspot.com",
+    messagingSenderId: "804837651025",
+    appId: "1:804837651025:web:4bfe7337def6b24c80be70"
+};
+export {firebaseConfig};
